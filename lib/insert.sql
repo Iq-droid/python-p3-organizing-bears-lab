1491ab2f673f46f0094fb77f36608e7a0fb0044d
Iqra Ali <iqra.ali@student.moringaschool.com>
2023-09-28 01:21:27 +0300

    oups

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, color, age, sex, temperament, alive) VALUES(1, 'Mr. Chocolate', 'Brown', 2, 'M', 'Friendly', 0);
+INSERT INTO bears (id, name, color, age, sex, temperament, alive) VALUES(2, 'Rowdy', 'White', 10, 'M', 'Tough', 1);
+INSERT INTO bears (id, name, color, age, sex, temperament, alive) VALUES(3, 'Tabitha', 'Black', 4, 'F', 'Outgoing', 1);
+INSERT INTO bears (id, name, color, age, sex, temperament, alive) VALUES(4, 'Sergent Brown', 'Brown', 5, 'M', 'Brave', 0);
+INSERT INTO bears (id, name, color, age, sex, temperament, alive) VALUES(5, 'Melissa', 'Blue', 9, 'F', 'Curious', 1);
+INSERT INTO bears (id, name, color, age, sex, temperament, alive) VALUES(6, 'Grinch', 'Amber', 3, 'M', 'Apt', 1);
+INSERT INTO bears (id, name, color, age, sex, temperament, alive) VALUES(7, 'Wendy', 'Green', 7, 'F', 'Lovely', 1);
+INSERT INTO bears (name) VALUES(NULL);
